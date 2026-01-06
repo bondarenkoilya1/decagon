8bdf1d7ee53cc2e7642b1a1c0212534cc97cbd20
Ilya Bondarenko <ib@chulakov.com>
2026-01-06 12:17:20 +0200

    feat: create basic canvas block with axis lines

---
+++ src/entities/vector/ui/coordinates-system/coordinates-system.tsx
@@ -0,0 +1,53 @@
+"use client";
+import type { JSX } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import type { ColorType } from "src/shared";
+
+const COLORS = {
+  grid: "#e8f4f8",
+  axes: "#2c3e50",
+  labels: "#34495e"
+};
+
+export const CoordinatesSystem = (): JSX.Element => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    const canvasRect = canvas.getBoundingClientRect();
+    const canvasWidth = canvasRect.width;
+    const canvasHeight = canvasRect.height;
+
+    drawAxis(ctx, canvasWidth, canvasHeight);
+  }, []);
+
+  const drawAxis = (
+    ctx: CanvasRenderingContext2D,
+    canvasWidth: number,
+    canvasHeight: number,
+    color: ColorType = "#ddd"
+  ): void => {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, canvasHeight / 2, canvasWidth, 1);
+    ctx.fillRect(canvasWidth / 2, 0, 1, canvasHeight);
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={700}
+      height={400}
+      className="bg-white border-2 border-solid border-[#3498db] rounded-lg shadow-md mx-auto"
+    />
+  );
+};
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -27,3 +27,5 @@ export type NavigationSectionProps = {
   title: string;
   items: NavigationItemProps[];
 };
+
+export type ColorType = `#${string}`;
