31473838f733448ea2fcf2505320152291f34e15
Ilya Bondarenko <ib@chulakov.com>
2026-01-31 15:44:50 +0300

    fix: set correct type for navigation items and sections

--- src/shared/types/navigation.ts
+++ src/shared/types/navigation.ts
@@ -1,9 +1,13 @@
+import type { useTranslations } from "next-intl";
+
+export type TranslationKey = Parameters<ReturnType<typeof useTranslations<never>>>[0];
+
 export type NavigationItemProps = {
-  title: string;
+  title: TranslationKey;
   url: string;
 };
 
 export type NavigationSectionProps = {
-  title: string;
+  title: TranslationKey;
   items: NavigationItemProps[];
 };
